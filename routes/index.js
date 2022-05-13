38a8112dec992be0a128bbaa0b324de7409ff92d
jeremy lorette <jeremy.lorette@outlook.com>
2022-05-13 16:14:36 +0200

    AD tweets feed by DB

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
-const router = require('express').Router();
-const api = require('./api')
+const router = require("express").Router();
+const api = require("./api");
+const Tweet = require("../database/models/tweet.model");
 
-router.use('/api' , api)
+router.use("/api", api);
 
-router.get('/tweet/new' , (req,res) => {
-  res.render('tweets/tweet-form')
-})
+router.get("/tweet/new", (req, res) => {
+  res.render("tweets/tweet-form");
+});
 
-router.get('/', (req, res) => {
-  res.render('tweets/tweet-list');
-})
+router.get("/", (req, res) => {
+  Tweet.find({})
+    .exec()
+    .then((tweets) => res.render("tweets/tweet-list", { tweets }));
+});
 
 module.exports = router;
