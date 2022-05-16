180f3aac9571f2ed3677371bd77f975f9d313a08
jeremy lorette <jeremy.lorette@outlook.com>
2022-05-16 23:35:42 +0200

    delete tweet

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 const router = require('express').Router();
-const api = require('./api');
-const Tweet = require('../database/models/tweet.model');
+const tweets = require('./tweets');
 
-router.use('/api', api);
 
-router.get('/tweet/new', (req, res) => {
-  res.render('tweets/tweet-form');
+router.use('/tweets', tweets);
+router.get('/', (req , res) => {
+  res.redirect('/tweets')
 })
 
-router.get('/', (req, res) => {
-  Tweet.find({})
-       .exec()
-       .then( tweets => res.render('tweets/tweet-list', { tweets }) )
-})
+
 
 module.exports = router;
---
+++ routes/tweets.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const Tweet = require("../database/models/tweet.model");
+const {
+  tweetList,
+  tweetNew,
+  tweetCreate,
+  tweetDelete
+} = require("../controllers/tweets.controller");
+
+router.get("/", tweetList);
+
+router.get("/new", tweetNew);
+
+router.post("/", tweetCreate);
+
+router.delete('/:tweetId', tweetDelete)
+
+module.exports = router;
